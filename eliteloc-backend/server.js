b65c8a169139602077a0c420d260c03308815766
Kwameh <kwamspam20@gmail.com>
2025-04-15 14:33:24 +0200

    Ajout du backend (routes, server et packages)
    Fais par Kwameh et Bilal

---
+++ eliteloc-backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use('/api/utilisateurs', require('./routes/utilisateurs'));
+//app.use('/api/voitures', require('./routes/voitures'));
+//app.use('/api/locations', require('./routes/locations'));
+
+// main route
+app.get('/', (req, res) => {
+    res.send('API EliteLoc fonctionne correctement');
+});
+
+// lancement du serveur
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur le port ${PORT}`);
+});
